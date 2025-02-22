979e631f41656b7d96563273829b9a2fbc411d87
PrajanBasnet <spiderbasnet140@gmail.com>
2025-02-22 23:00:38 +0545

    print logic

--- main.js
+++ main.js
@@ -1,51 +1,50 @@
 let dialog = document.querySelector("dialog");
-let showBox= document.querySelector("dialog + button");
+let showBox = document.querySelector("dialog + button");
 let close = document.querySelector("dialog button");
 let form = document.querySelector("form");
-let mainpannel = document.querySelector(".mainpannel");
 let libary = [];
 
 
-function Book(bookname, author,read){
+function Book(bookname, author, read) {
     this.bookname = bookname;
     this.author = author;
     this.read = read;
-    this.printValue = function(){
-        console.log(this.bookname,this.author,this.read);
+    this.printValue = function () {
+        console.log(this.bookname, this.author, this.read);
     }
-}   
+}
 
 let dataKey = 0;
-function addToLibary(bookname,author,read){
-    let book = new Book(bookname,author,read);
-     libary.push(book);
-
+function addToLibary(bookname, author, read) {
+    let book = new Book(bookname, author, read);
+    libary.push(book);
+    let mainpannel = document.querySelector(".mainpannel");
+    mainpannel.innerHTML = "";
+    for (let i = 0; i < libary.length; i++) {
         let wrapper = document.createElement("div");
-        let insertData = document.createTextNode(
-            `${libary.bookname} - ${libary.author}`
-        )
+        wrapper.className = "card";
+        let insertData = document.createTextNode(`${libary[i].bookname} - ${libary[i].author}`)
         wrapper.appendChild(insertData);
         mainpannel.appendChild(wrapper);
-     
 
-     console.log(libary.length)
+    }
 }
 
 
 
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e) => {
     let bookName = document.getElementById("bname").value;
     let author = document.getElementById("author").value;
 
     e.preventDefault();
-     addToLibary(bookName,author,true);
+    addToLibary(bookName, author, true);
     dialog.close();
 })
-showBox.addEventListener("click",()=>{
+showBox.addEventListener("click", () => {
     dialog.showModal()
 });
 
-close.addEventListener("click",()=>{
+close.addEventListener("click", () => {
     dialog.close()
 })
 
