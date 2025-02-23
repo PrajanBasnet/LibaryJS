ec77377792e3b66f4b6244654402b2ee5c138af2
PrajanBasnet <spiderbasnet140@gmail.com>
2025-02-23 14:39:25 +0545

    read only

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let dialog = document.querySelector("dialog");
-let showBox = document.querySelector("dialog + button");
+let showBox = document.querySelector(".addBook");
 let close = document.querySelector("dialog button");
 let form = document.querySelector("form");
+let mainpannel = document.querySelector(".mainpannel");
+
+
 let libary = [];
 
 
@@ -9,54 +12,66 @@ function Book(bookname, author, read) {
     this.bookname = bookname;
     this.author = author;
     this.read = read;
-    this.printValue = function () {
-        console.log(this.bookname, this.author, this.read);
-    }
+
+}
+Book.prototype.readvalue = function(){
+     this.read;
+     let checkbutton = document.createElement("input");
+     checkbutton.type = "checkbox";
+     checkbutton.checked = this.read;
 }
 
+
 function addToLibary(bookname, author, read) {
+    mainpannel.innerHTML = "";
     let book = new Book(bookname, author, read);
     libary.push(book);
-    let mainpannel = document.querySelector(".mainpannel");
-    mainpannel.innerHTML = "";
-
+    book.readvalue();
+    
     for (let i = 0; i < libary.length; i++) {
         let wrapper = document.createElement("div");
+        let checkbutton = document.createElement("input");
         wrapper.className = "card";
         let insertData = document.createTextNode(`${libary[i].bookname} - ${libary[i].author}`)
         let createButton = document.createElement("button");
         createButton.className = "btn";
         let text = document.createTextNode("Button");
+    
+        checkbutton.type = "checkbox";
+        checkbutton.checked = book.readvalue();
+
         createButton.addEventListener("click", () => {
             let key = parseInt(createButton.dataset.dataId)
-            console.log(libary);
             libary.splice(key, 1);
             mainpannel.removeChild(wrapper);
 
         })
-
+        
         createButton.dataset.dataId = i;
         createButton.appendChild(text);
         wrapper.appendChild(createButton);
+        wrapper.appendChild(checkbutton);
 
+        
         wrapper.appendChild(insertData);
         mainpannel.appendChild(wrapper);
-
-
-        console.log(wrapper);
-        console.log(createButton);
+        
     }
 
 }
+Object.setPrototypeOf(Book.prototype,addToLibary.prototype)
 
 
 
 form.addEventListener("submit", (e) => {
     let bookName = document.getElementById("bname").value;
     let author = document.getElementById("author").value;
+    let readvalue = document.querySelector("#read");
 
     e.preventDefault();
-    addToLibary(bookName, author, true);
+
+    // if(readd.value == "o")
+    addToLibary(bookName, author, readvalue.checked);
     dialog.close();
 })
 showBox.addEventListener("click", () => {
